e291e3831b871909c86c0ec25ba05961f0b043ec
tacticalParmesan <edoardoferreri10@gmail.com>
2024-04-04 22:42:33 +0200

    Added styling to the landing page and to the dialog; changed a little the board and created an aoutonomous board.css file for more order

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 const Gameboard = (function () {
-	
 	let board = [
 		[, , ,],
 		[, , ,],
 		[, , ,],
 	];
 
-
 	function updateBoard(mark, row, column) {
-		board[row][column] = mark; 
+		board[row][column] = mark;
 	}
 
 	function isSpotEmpty(row, column) {
@@ -30,16 +28,12 @@ const Gameboard = (function () {
 	return { updateBoard, clearBoard, getBoard, isSpotEmpty };
 })();
 
-
 const GameController = (function () {
-
 	let players = [];
 	let currentPlayer = {};
-    let turnCount = 0;
-
+	let turnCount = 0;
 
 	function getPlayers() {
-
 		const playerOneName = DisplayController.modal.playerOneNameField.value;
 		const playerTwoName = DisplayController.modal.playerTwoNameField.value;
 		const playerOneMark = DisplayController.modal.playerOneMarkField.value;
@@ -51,11 +45,9 @@ const GameController = (function () {
 		players.push(newPlayerOne, newPlayerTwo);
 
 		startGame();
-
 	}
 
 	function startGame() {
-
 		currentPlayer = players[1];
 		updateCurrentPlayer();
 		turnCount = 0;
@@ -64,10 +56,12 @@ const GameController = (function () {
 		createLogicBoard();
 		DisplayController.boardFunctions.createUIBoard();
 
-		if(document.getElementById("start-game-btn")){
-			DisplayController.DOMElements.body.removeChild(DisplayController.DOMElements.playButton);
+		if (document.getElementById("start-game-btn")) {
+			DisplayController.DOMElements.mainElement
+	.removeChild(
+				DisplayController.DOMElements.playButton
+			);
 		}
-		
 	}
 
 	function createLogicBoard() {
@@ -75,44 +69,35 @@ const GameController = (function () {
 	}
 
 	function playTurn(row, col) {
-
 		if (Gameboard.isSpotEmpty(row, col)) {
-
 			// Current player places its mark and passes the turn
 			currentPlayer.placeMark(currentPlayer.mark, row, col);
 			updateCurrentPlayer();
 
 			// The logic check if there is a winner and moves onto the next turn
 			DisplayController.boardFunctions.updateUIBoard();
-            turnCount++;
+			turnCount++;
 			checkWinner();
-
 		} else {
-
 			alert("This spot is already occupied!");
-
 		}
 	}
 
 	function updateCurrentPlayer() {
-		
 		// Changes the current player and updates the display text
 		currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
-		DisplayController.DOMElements.turnTracker.textContent = "It's " + currentPlayer.name + "'s turn!";
-
+		DisplayController.DOMElements.turnTracker.textContent =
+			"It's " + currentPlayer.name + "'s turn!";
 	}
 
 	function checkWinner() {
-
 		// Gets a copy of the board and performs checks to see if there is a winner
 		const board = Gameboard.getBoard();
 		let winner;
 
 		function checkByRow() {
-			
 			for (const row of board) {
 				if (row[0] === row[1] && row[1] === row[2] && row[0] !== null) {
-
 					winner =
 						row[0] === players[0].mark ? players[0].name : players[1].name;
 				}
@@ -120,10 +105,12 @@ const GameController = (function () {
 		}
 
 		function checkByColumn() {
-
 			for (let i = 0; i < 3; i++) {
-				if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== null) {
-
+				if (
+					board[0][i] === board[1][i] &&
+					board[1][i] === board[2][i] &&
+					board[0][i] !== null
+				) {
 					winner =
 						board[0][i] === players[0].mark ? players[0].name : players[1].name;
 				}
@@ -131,90 +118,85 @@ const GameController = (function () {
 		}
 
 		function checkDiagonally() {
-
 			if (
-				(board[0][0] === board[1][1] &&	board[1][1] === board[2][2] && board[1][1] !== null) ||
-				(board[0][2] === board[1][1] &&	board[1][1] === board[2][0] && board[1][1] !== null)) {
-
+				(board[0][0] === board[1][1] &&
+					board[1][1] === board[2][2] &&
+					board[1][1] !== null) ||
+				(board[0][2] === board[1][1] &&
+					board[1][1] === board[2][0] &&
+					board[1][1] !== null)
+			) {
 				winner =
 					board[1][1] === players[0].mark ? players[0].name : players[1].name;
 			}
 		}
 
-        function isTie(){
-
+		function isTie() {
 			// Checks is there is no winner and the maximun of turns has passed (9)
-            if(turnCount === 9 && !winner) return true; 
-        }
+			if (turnCount === 9 && !winner) return true;
+		}
 
 		function checkResult() {
-
 			// Checks for different win conditions...
 			checkByRow();
 			checkByColumn();
 			checkDiagonally();
 
 			//...and declares a winner if there is one!
-			if(winner){
+			if (winner) {
 				endGame(winner);
 			} else if (isTie()) {
-				endGame()
+				endGame();
 			}
 		}
 
 		checkResult();
-
 	}
 
-	function endGame(winner=undefined) {
-
+	function endGame(winner = undefined) {
 		// 1 | Disables the gameboard
-		DisplayController.boardFunctions.disableUIBoard()
+		DisplayController.boardFunctions.disableUIBoard();
 
 		// 2 | Deletes the turn tracking text
-		DisplayController.DOMElements.body.removeChild(DisplayController.DOMElements.turnTracker)
+		DisplayController.DOMElements.mainElement
+.removeChild(
+			DisplayController.DOMElements.turnTracker
+		);
 
 		// 3 | Displays the winner of the game
-		if(winner) {
-			DisplayController.displayFunctions.displayWinner(winner)
+		if (winner) {
+			DisplayController.displayFunctions.displayWinner(winner);
 		} else {
-			DisplayController.displayFunctions.displayTie("It's a tie!")
+			DisplayController.displayFunctions.displayTie("It's a tie!");
 		}
 
 		// 4 | Shows result and button for restart
-		DisplayController.displayFunctions.showRestartButton() // TODO
-
+		DisplayController.displayFunctions.showRestartButton(); // TODO
 	}
 
 	function restart() {
-
 		// Starts a new game
 		resetGameData();
 		DisplayController.boardFunctions.resetUIBoard();
 		startGame();
-		
 	}
 
 	function resetGameData() {
-
 		winner = undefined;
 		turnCount = 0;
-
 	}
 
 	return { getPlayers, startGame, playTurn, restart };
 })();
 
-
 const DisplayController = (function () {
-
 	// User Interface References
-	const body = document.querySelector("body");
+	const mainElement = document.querySelector("main");
 	let boardContainer;
-	
+
 	// Dialog references
 	const newPlayersModal = document.querySelector("dialog");
-	const newPlayersForm = document.querySelector("dialog > form")
+	const newPlayersForm = document.querySelector("dialog > form");
 	const playerOneNameField = document.querySelector("#player-one-name");
 	const playerOneMarkField = document.querySelector("#player-one-mark");
 	const playerTwoNameField = document.querySelector("#player-two-name");
@@ -225,18 +207,22 @@ const DisplayController = (function () {
 		GameController.getPlayers();
 		newPlayersModal.close();
 		newPlayersForm.reset();
-	})
+	});
+	const closeModalButton = document.querySelector("#close-modal-btn");
+	closeModalButton.addEventListener("mousedown", () => {
+		newPlayersModal.close();
+		newPlayersForm.reset();
+	});
 
 	const playButton = document.querySelector("#start-game-btn");
-	playButton.addEventListener("click",() => newPlayersModal.showModal());
-	
+	playButton.addEventListener("click", () => newPlayersModal.showModal());
+
 	const turnTracker = document.createElement("p");
 	turnTracker.id = "turn-tracker";
 
 	let boardUIArray = []; // NodeList to store the spots on the UI board
 
 	function createUIBoard() {
-
 		// 1 | Creates the grid
 		boardContainer = document.createElement("div");
 		boardContainer.classList.add("board-container");
@@ -244,7 +230,6 @@ const DisplayController = (function () {
 		// 2 | Creates the board one row at a time
 		for (let i = 0; i < 3; i++) {
 			for (let j = 0; j < 3; j++) {
-
 				const newSpot = document.createElement("button");
 				newSpot.classList.add("board-spot");
 				newSpot.setAttribute("row", i);
@@ -255,8 +240,10 @@ const DisplayController = (function () {
 		}
 
 		// 3 | Appends the newly created spots to the grid and stores them in the NodeList
-		body.appendChild(boardContainer);
-		body.appendChild(turnTracker);
+		mainElement
+.appendChild(boardContainer);
+		mainElement
+.appendChild(turnTracker);
 		boardUIArray = document.querySelectorAll(".board-spot");
 
 		// 4 | Loops over the NodeList to attach to each an event listener that reads the spot's coordinates to interact with the logic board
@@ -264,11 +251,9 @@ const DisplayController = (function () {
 	}
 
 	function loadClickListeners() {
-
 		// As per point 4 of the createUIBoard function
 
 		for (const spot of boardUIArray) {
-
 			const spotRow = spot.getAttribute("row");
 			const spotCol = spot.getAttribute("col");
 
@@ -279,10 +264,8 @@ const DisplayController = (function () {
 	}
 
 	function updateUIBoard() {
-
 		// Updates the UI displaying the marks placed in the spots
 		for (const spot of boardUIArray) {
-
 			// Does so by getting the corresponding coordinates from UI grid...
 			const spotRow = spot.getAttribute("row");
 			const spotCol = spot.getAttribute("col");
@@ -293,70 +276,81 @@ const DisplayController = (function () {
 	}
 
 	function disableUIBoard() {
-
 		// Disables the buttons, no marks can be placed anymore
-		boardUIArray.forEach( (btn) => {
+		boardUIArray.forEach((btn) => {
 			btn.disabled = true;
 		});
 	}
 
 	function enableUIBoard() {
-
 		// Disables the buttons, no marks can be placed anymore
-		boardUIArray.forEach( (btn) => {
+		boardUIArray.forEach((btn) => {
 			btn.disabled = false;
 		});
 	}
 
 	function displayWinner(winnerPlayer) {
-
 		const winnerText = document.createElement("p");
 		winnerText.id = "result-text";
-		winnerText.textContent = `${winnerPlayer} wins the game!`
+		winnerText.textContent = `${winnerPlayer} wins the game!`;
 
-		body.appendChild(winnerText);
+		mainElement
+.appendChild(winnerText);
 	}
 
 	function displayTie() {
-
 		const tieText = document.createElement("p");
 		tieText.id = "result-text";
 		tieText.textContent = `It's a tie!`;
 
-		body.appendChild(tieText);
+		mainElement
+.appendChild(tieText);
 	}
 
 	function showRestartButton() {
-
-		const restartButton = document.createElement("button")
+		const restartButton = document.createElement("button");
 		restartButton.id = "restart-btn";
 		restartButton.textContent = "New Game?";
 		restartButton.addEventListener("mousedown", GameController.restart);
-		body.appendChild(restartButton);
-
+		mainElement
+.appendChild(restartButton);
 	}
 
 	function resetUIBoard() {
-
-		body.removeChild(boardContainer);
-		body.removeChild(document.querySelector("#result-text"));
-		body.removeChild(document.querySelector("#restart-btn"));
-
-		
+		mainElement
+.removeChild(boardContainer);
+		mainElement
+.removeChild(document.querySelector("#result-text"));
+		mainElement
+.removeChild(document.querySelector("#restart-btn"));
 	}
 
-	return { DOMElements: {body, playButton, turnTracker},
-			 boardFunctions: {createUIBoard, updateUIBoard, disableUIBoard, enableUIBoard, resetUIBoard},
-			 displayFunctions: {displayWinner, displayTie, showRestartButton},
-			 modal: {newPlayersModal, playerOneNameField, playerOneMarkField, playerTwoNameField, playerTwoMarkField, startGameButton}  };
+	return {
+		DOMElements: { mainElement
+	, playButton, turnTracker },
+		boardFunctions: {
+			createUIBoard,
+			updateUIBoard,
+			disableUIBoard,
+			enableUIBoard,
+			resetUIBoard,
+		},
+		displayFunctions: { displayWinner, displayTie, showRestartButton },
+		modal: {
+			newPlayersModal,
+			playerOneNameField,
+			playerOneMarkField,
+			playerTwoNameField,
+			playerTwoMarkField,
+			startGameButton,
+		},
+	};
 })();
 
-function Player(name, mark){
-	
-	function placeMark(mark, row, column){
-		Gameboard.updateBoard(mark, row, column)
+function Player(name, mark) {
+	function placeMark(mark, row, column) {
+		Gameboard.updateBoard(mark, row, column);
 	}
 
-	return { name, mark, placeMark }
-
-};
+	return { name, mark, placeMark };
+}
